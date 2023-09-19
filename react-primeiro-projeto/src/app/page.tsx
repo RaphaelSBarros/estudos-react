7a1712fd9f66553ebc8712af4f38316cf2cce068
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-09-19 10:57:43 -0300

    Exportando e importando um componente

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Square, Circle } from '@/components/Shapes';
+
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
+      
+      <Square/>
+      <Circle/>
     </div>
   );
 }
