3d160fcf9dbc45be494648b5124d27014932c299
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-10-06 13:51:02 -0300

    Passando props para um componente

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -6,7 +6,16 @@ const Page = () => {
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
       
-      <Person />
+      <Person
+        name="Elon Musk"
+        avatar="https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg"
+        roles={['CEO da Tesla', 'CEO da SpaceX']}
+      />
+      <Person
+        name="Jeff Bezos"
+        avatar="https://classic.exame.com/wp-content/uploads/2021/09/GettyImages-1036099078-1.jpg?quality=70&strip=info&w=1024"
+        roles={['CEO da Amazon', 'CEO da Blue Origin']}
+      />
     </div>
   );
 }
--- react-primeiro-projeto/src/components/Person.tsx
+++ react-primeiro-projeto/src/components/Person.tsx
@@ -1,31 +1,31 @@
+import { type } from "os";
+
 const getWeekday = (today: Date) => {
     return new Intl.DateTimeFormat('pt-BR', { weekday: 'long'}).format(today);
 }
 
-export const Person = () => {
+type Props = {
+    name: string,
+    avatar: string,
+    roles: string[]
+}
 
-    const data = {
-        name: 'Elon Musk',
-        avatar: 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg',
-        roles: ['CEO da Tesla', 'CEO da SpaceX']
-    }
+export const Person = ({ name, avatar, roles }: Props) => {
 
-    //const name: string = 'Elon Musk';
-    //const avatar: string = 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg';
-    const today: Date = new Date();
+
 
     return(
-        <>
-            <h1 style={{ color: 'red', fontSize: '30px' }}>{data.name} - {getWeekday(today)}</h1>
+        <div className="p-3">
+            <h1>{name}</h1>
             <img 
-                src={data.avatar}
-                alt={data.name}
+                src={avatar}
+                alt={name}
                 className="w-40"
             />
             <ul>
-                <li>{data.roles[0]}</li>
-                <li>{data.roles[1]}</li>
+                <li>{roles[0]}</li>
+                <li>{roles[1]}</li>
             </ul>
-        </>
+        </div>
     );
 }
