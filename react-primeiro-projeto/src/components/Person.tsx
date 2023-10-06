d7ee37bfc97db38d17136e757ccfc4b9b547a15e
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-10-06 13:27:06 -0300

    variáveis, funções e objetos em jsx

--- react-primeiro-projeto/src/components/Person.tsx
+++ react-primeiro-projeto/src/components/Person.tsx
@@ -1,16 +1,30 @@
+const getWeekday = (today: Date) => {
+    return new Intl.DateTimeFormat('pt-BR', { weekday: 'long'}).format(today);
+}
+
 export const Person = () => {
+
+    const data = {
+        name: 'Elon Musk',
+        avatar: 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg',
+        roles: ['CEO da Tesla', 'CEO da SpaceX']
+    }
+
+    //const name: string = 'Elon Musk';
+    //const avatar: string = 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg';
+    const today: Date = new Date();
+
     return(
         <>
-            <h1>Elon Musk</h1>
+            <h1 style={{ color: 'red', fontSize: '30px' }}>{data.name} - {getWeekday(today)}</h1>
             <img 
-                src="https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg"
-                alt="Elon Musk"
+                src={data.avatar}
+                alt={data.name}
                 className="w-40"
             />
             <ul>
-                <li>CEO da Tesla</li>
-                <li>CEO da SpaceX</li>
-                <li>CEO da ...</li>
+                <li>{data.roles[0]}</li>
+                <li>{data.roles[1]}</li>
             </ul>
         </>
     );
