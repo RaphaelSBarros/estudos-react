ebf2bf49c80b721a78798bbcae7fc225770fbdde
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-10-07 09:56:12 -0300

    Exercício Greeting Condicional 1

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,24 +1,25 @@
-import { peopleList } from "@/data/peopleList";
-
 const Page = () => {
 
-  const chemists = peopleList.filter(person => person.profession === 'chemist');
+  const fulltime = new Intl.DateTimeFormat('pt-BR', {
+    timeStyle: 'short',
+    hour12: false
+  }).format();
+
+  const hour = new Date().getHours();
+  let greeting = '';
+
+  if(hour >= 0 && hour < 12){
+    greeting = 'Bom dia';
+  }else if(hour >= 12 && hour < 18){
+    greeting = 'Boa tarde';
+  }else if(hour >= 18 && hour <= 23){
+    greeting = 'Boa noite';
+  }
 
   return (
-    <div>
-      <h1 className="font-bold text-2xl">Olá Mundo</h1>
-      <h3>Algum outro texto</h3>
-
-      {chemists.length > 0 &&
-        <>
-          <ul>
-            <h3>Lista de químicos</h3>
-            {chemists.map(person =>
-              <li key={person.id}>{person.name}</li>
-            )}
-          </ul>
-        </>
-      }
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl">{fulltime}</div>
+      <div className="text-5xl font-bold">{greeting}</div>
     </div>
   );
 }
