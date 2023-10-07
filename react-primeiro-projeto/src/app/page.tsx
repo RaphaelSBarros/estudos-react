73d5bea0c4a93a45378add05b422355093fda59e
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-10-07 10:01:26 -0300

    Exercício Greeting Condicional 2

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -6,20 +6,15 @@ const Page = () => {
   }).format();
 
   const hour = new Date().getHours();
-  let greeting = '';
-
-  if(hour >= 0 && hour < 12){
-    greeting = 'Bom dia';
-  }else if(hour >= 12 && hour < 18){
-    greeting = 'Boa tarde';
-  }else if(hour >= 18 && hour <= 23){
-    greeting = 'Boa noite';
-  }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fulltime}</div>
-      <div className="text-5xl font-bold">{greeting}</div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour < 12 && "Bom dia"}
+        {hour >= 12 && hour < 18 && "Bom tarde"}
+        {hour >= 18 && hour <= 23 && "Bom noite"}
+      </div>
     </div>
   );
 }
