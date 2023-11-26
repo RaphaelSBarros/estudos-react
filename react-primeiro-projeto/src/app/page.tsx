32b27287b8108da30b0c4e1f9d9a0e2c31949729
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-11-26 18:09:42 -0300

    Atualizando Objetos em States

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,21 +1,37 @@
 "use client"
 
+import { Person } from "@/types/Person";
 import { useState } from "react";
 
 const Page = () => {
 
-  const [count, setCount] = useState(0);
+  const [fullName, setFullName] = useState<Person>({ name: '', lastName: ''});
 
-  const handleBtnClick = () => {
-    setCount(count +2);
-    setCount(c => c + 2);
-    setCount(c => c + 2);
+  const handleClearButton = () => {
+    setFullName({ name: '', lastName: '' })
   }
-  
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <p>{count}</p>
-      <button onClick={handleBtnClick} className="bg-blue-700 text-white p-3 rounded-md">+6</button>
+      <input
+        type="text"
+        placeholder="Nome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3" 
+        value={fullName.name}
+        onChange={e => setFullName({ ...fullName, name: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Sobrenome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3" 
+        value={fullName.lastName}
+        onChange={e => setFullName({ ...fullName, lastName: e.target.value})}
+      />
+
+      <p>Meu nome completo é:</p>
+      <p>{fullName.name} {fullName.lastName}</p>
+
+      <button onClick={handleClearButton} className="bg-blue-700 rounded-md p-3 mt-2">Limpar Tudo</button>
     </div>
   );
 }
