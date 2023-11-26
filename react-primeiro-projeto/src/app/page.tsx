1fc4b872694efcd5512c0fc921220706c75b6576
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-11-26 17:20:54 -0300

    State Updater

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -8,12 +8,14 @@ const Page = () => {
 
   const handleBtnClick = () => {
     setCount(count +2);
+    setCount(c => c + 2);
+    setCount(c => c + 2);
   }
   
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
       <p>{count}</p>
-      <button onClick={handleBtnClick} className="bg-blue-700 text-white p-3 rounded-md">+2</button>
+      <button onClick={handleBtnClick} className="bg-blue-700 text-white p-3 rounded-md">+6</button>
     </div>
   );
 }
