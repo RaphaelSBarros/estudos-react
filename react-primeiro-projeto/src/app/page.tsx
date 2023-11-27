efa10637197698fd0056a9c78082b880de6181bb
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-11-26 23:43:03 -0300

    CRUD utilizando Arrays em States

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,37 +1,56 @@
 "use client"
 
-import { Person } from "@/types/Person";
+import { TodoItem } from "@/types/TodoItem";
 import { useState } from "react";
 
 const Page = () => {
 
-  const [fullName, setFullName] = useState<Person>({ name: '', lastName: ''});
+  const [itemInput, setItemInput] = useState('');
 
-  const handleClearButton = () => {
-    setFullName({ name: '', lastName: '' })
+  const [list, setList] = useState<TodoItem[]>([
+    { label: 'Fazer dever de casa', checked: true },
+    { label: 'Comprar o bolo', checked: false }
+  ]);
+
+  const handleAddButton = () => {
+    if(itemInput.trim() === '') return;
+    setList([...list, { label: itemInput, checked: false}]);
+    setItemInput('');
+  };
+
+  const deleteItem = (index: number) => {
+    setList(list.filter((item, key) => key !== index))
+  }
+
+  const toggleItem = (index: number) => {
+    let newList = [...list];
+    newList[index].checked = !newList[index].checked;
+
+    setList(newList);
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <input
-        type="text"
-        placeholder="Nome"
-        className="border border-black p-3 text-2xl text-black rounded-md mb-3" 
-        value={fullName.name}
-        onChange={e => setFullName({ ...fullName, name: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Sobrenome"
-        className="border border-black p-3 text-2xl text-black rounded-md mb-3" 
-        value={fullName.lastName}
-        onChange={e => setFullName({ ...fullName, lastName: e.target.value})}
-      />
+    <div className="w-screen h-screen flex flex-col items-center text-2xl">
+      <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
 
-      <p>Meu nome completo é:</p>
-      <p>{fullName.name} {fullName.lastName}</p>
+      <div className="flex w-full max-w-lg my-3 p-3 rounded-md bg-gray-700 border-2 border-gray-50">
+        <input
+          type="text"
+          placeholder="O que deseja fazer?"
+          className="flex-1 border border-black p-3 text-2xl text-black rounded-md mr-3"
+          onChange={e => setItemInput(e.target.value)}
+        />
+        <button onClick={handleAddButton} className="bg-blue-700 rounded-md p-3">Adicionar</button>
+      </div>
 
-      <button onClick={handleClearButton} className="bg-blue-700 rounded-md p-3 mt-2">Limpar Tudo</button>
+      <ul className="w-full max-w-lg list-disc pl-5">
+        {list.map((item, index) => (
+          <li key={index}>
+            <input onClick={() => toggleItem(index)} type="checkbox" checked={item.checked} className="w-6 h-6 mr-3"/>
+            {item.label} - <button onClick={() => deleteItem(index)} className="hover:underline">[ deletar ]</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
