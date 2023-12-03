4a218fe146288d0acfb1446407268129745c301a
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-12-03 17:40:29 -0300

    Exercício de Quiz finalizado

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,43 +1,61 @@
 "use client"
 
-import { Modal } from "@/components/Modal";
-import { PhotoItem } from "@/components/PhotoItem";
-import { photoList } from "@/data/photoList";
+import { QuestionItem } from "@/components/QuestionItem";
+import { Results } from "@/components/Results";
+import { questions } from "@/data/questions";
 import { useState } from "react";
 
 const Page = () => {
+  const [answers, setAnswers] = useState<number[]>([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
 
-  const [showModal, setShowModal] = useState(false);
-  const [imageOfModal, setImageOfModal] = useState('');
+  const title = 'Quiz de Culinária';
 
-  const openModal = (id:number) => {
-    const photo = photoList.find(item => item.id === id);
-    if (photo){
-      setImageOfModal(photo.url);
-      setShowModal(true);
+  const loadNextQuestion = () => {
+    if(questions[currentQuestion+1]){
+      setCurrentQuestion(currentQuestion+1);
+    }else {
+      setShowResult(true);
     }
   }
 
-  const closeModal = () => {
-    setShowModal(false);
+  const handleAnswered = (answer: number) => {
+    setAnswers([ ...answers, answer ]);
+    loadNextQuestion();
+  };
+
+  const handleRestartButton = () => {
+    setAnswers([]);
+    setCurrentQuestion(0);
+    setShowResult(false);
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center text-2xl">
-      <h1 className="text-center text-3xl font-bold my-10">Fotos Intergalácticas</h1>
-      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {photoList.map(item => (
-          <PhotoItem
-            key={item.id}
-            photo={item}
-            onClick={() => openModal(item.id)}
-          />
-        ))}
-      </section>
-      {showModal &&
-        <Modal image={imageOfModal} closeModal={closeModal}
-        />
-      }
+    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
+        <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
+        <div className="p-5">
+          {!showResult &&
+            <QuestionItem 
+              question={questions[currentQuestion]}
+              count={currentQuestion+1}
+              onAnswer={handleAnswered}
+            />
+          }
+          {showResult &&
+            <Results questions={questions} answers={answers}/>
+          }
+        </div>
+        <div className="p-5 text-center border-t border-gray-300">
+          {!showResult &&
+            `${currentQuestion+1} de ${questions.length} pergunta${questions.length === 1 ? '':'s'}`
+          }
+          {showResult &&
+            <button onClick={handleRestartButton} className="px-3 py-2 rounded-md bg-blue-800 text-white">Reiniciar Quiz</button>
+          }
+        </div>
+      </div>
     </div>
   );
 }
---
+++ react-primeiro-projeto/src/data/questions.ts
@@ -0,0 +1,23 @@
+import { Question } from "@/types/Question"
+export const questions: Question[] = [
+    {
+        question: 'Qual ingrediente não pode faltar?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Água',
+            'Açúcar'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual melhor método de cozinhar?',
+        options: [
+            'Micro-ondas',
+            'Fogão',
+            'Fogueira',
+            'Lança-chamas'
+        ],
+        answer: 3
+    }
+]
