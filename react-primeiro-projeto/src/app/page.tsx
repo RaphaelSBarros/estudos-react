3001a25d573b55393e6fb93f13a28a9b46e26a0a
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-23 11:56:46 -0300

    Exercício: Galeria de Fotos 1

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,59 +1,18 @@
 "use client"
 
-import { TodoItem } from "@/types/TodoItem";
-import { useState } from "react";
+import { photoList } from "@/data/photoList";
 
 const Page = () => {
 
-  const [itemInput, setItemInput] = useState('');
-
-  const [list, setList] = useState<TodoItem[]>([
-    { id: 1, label: 'Fazer dever de casa', checked: true },
-    { id: 2, label: 'Comprar o bolo', checked: false }
-  ]);
-
-  const handleAddButton = () => {
-    if(itemInput.trim() === '') return;
-    setList([...list, { id: list.length+1, label: itemInput, checked: false}]);
-    setItemInput('');
-  };
-
-  const deleteItem = (id: number) => {
-    setList(list.filter(item => item.id !== id));
-  }
-
-  const toggleItem = (id: number) => {
-    let newList = [...list];
-    for(let i in newList){
-      if(newList[i].id === id){
-        newList[i].checked = !newList[i].checked;
-      }
-    }
-    setList(newList);
-  }
-
   return (
-    <div className="w-screen h-screen flex flex-col items-center text-2xl">
-      <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
+    <div className="mx-2">
+      <h1 className="text-center text-3xl font-bold my-10">Fotos Intergalácticas</h1>
 
-      <div className="flex w-full max-w-lg my-3 p-3 rounded-md bg-gray-700 border-2 border-gray-50">
-        <input
-          type="text"
-          placeholder="O que deseja fazer?"
-          className="flex-1 border border-black p-3 text-2xl text-black rounded-md mr-3"
-          onChange={e => setItemInput(e.target.value)}
-        />
-        <button onClick={handleAddButton} className="bg-blue-700 rounded-md p-3">Adicionar</button>
-      </div>
-
-      <ul className="w-full max-w-lg list-disc pl-5">
-        {list.map(item => (
-          <li key={item.id}>
-            <input onClick={() => toggleItem(item.id)} type="checkbox" checked={item.checked} className="w-6 h-6 mr-3"/>
-            {item.label} - <button onClick={() => deleteItem(item.id)} className="hover:underline">[ deletar ]</button>
-          </li>
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {photoList.map(item => (
+          <div>...</div>
         ))}
-      </ul>
+      </section>
     </div>
   );
 }
---
+++ react-primeiro-projeto/src/data/photoList.ts
@@ -0,0 +1,13 @@
+import { Photo } from "@/types/Photo";
+
+export const photoList: Photo[] = [
+    { id:1, url: '1.jpg' },
+    { id:1, url: '2.jpg' },
+    { id:1, url: '3.jpg' },
+    { id:1, url: '4.jpg' },
+    { id:1, url: '5.jpg' },
+    { id:1, url: '6.jpg' },
+    { id:1, url: '7.jpg' },
+    { id:1, url: '8.jpg' },
+    { id:1, url: '9.jpg' }
+]
