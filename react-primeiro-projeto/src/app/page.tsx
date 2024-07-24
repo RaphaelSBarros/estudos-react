251016e108be7eb884ec3df08138ae97708a5368
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-24 14:41:58 -0300

    Usando o CleanUp

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,23 +1,16 @@
 "use client"
 
-import { VideoPlayer } from "@/components/VideoPlayer";
-import { useEffect, useState } from "react";
+import { Square } from "@/components/Square";
+import { useState } from "react";
 
 const Page = () => {
-  const [playing, setPlaying] = useState(false)
+  const [show, setShow] = useState(false)
 
   return (
     <div className="">
-      <div className="border border-white p-3 mb-4">
-        <p className="text-2xl text-blue-400 mb-3">{playing ? 'RODANDO' : 'PAUSADO'}</p>
-        <button className="bg-blue-400 rounded-md p-3 text-black" onClick={() => setPlaying(!playing)}>Play/Pause</button>
-      </div>
-
-      <VideoPlayer
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-        isPlaying={playing}
-      />
-
+      <button onClick={() => setShow(!show)}>Mostrar/Ocultar</button>
+      
+      {show && <Square />}
     </div>
   );
 }
