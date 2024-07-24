e2681f3288728acbb5deca8bc7f3d37ee97ee803
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-24 11:14:55 -0300

    Aprendendo o que é e omo utilizar o Effect

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,62 +1,23 @@
 "use client"
 
-import { QuestionItem } from "@/components/QuestionItem";
-import { Results } from "@/components/Results";
-import { questions } from "@/data/questions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Page = () => {
-  const [answers, setAnswers] = useState<number[]>([]);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showResult, setShowResult] = useState(false);
+  const [name, setName] = useState('Raphael');
+  const [age, setAge] = useState(90);
 
-  const title = "Quiz de Culinária";
-
-  const loadNextQuestion = () => {
-    if(questions[currentQuestion+1]){
-      setCurrentQuestion(currentQuestion+1)
-    } else{
-      setShowResult(true);
-    }
-  }
-
-  const handleAnswered = (answer: number) => {
-    setAnswers([...answers, answer]);
-    loadNextQuestion();
-  }
-
-  const handleRestartButton = () => {
-    setAnswers([]);
-    setCurrentQuestion(0);
-    setShowResult(false);
-  }
+  useEffect(() => {
+    console.log('RODOU O EFFECT...')
+  }, [age])
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
-      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
-        <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
-        <div className="p-5">
-          {!showResult &&
-            <QuestionItem
-              question={questions[currentQuestion]}
-              count={currentQuestion + 1}
-              onAnswer={handleAnswered}
-            />
-          }
-          {showResult &&
-            <Results questions={questions} answers={answers} />
-          }
-        </div>
-        <div className="p-5 text-center border-gray-300">
-          {!showResult && 
-            `${currentQuestion +1} de ${questions.length} pergunta${questions.length === 1 ? '' : 's'}`
-          }
-          {showResult &&
-            <button onClick={handleRestartButton} className="px-3 py-2 rounded-md bg-blue-800 text-white">Reiniciar Quiz</button>
-          }
-
-        </div>
-      </div>
+    <div className="">
+      <p>Meu nome é {name} e eu tenho {age} anos.</p>
+      <hr />
+      <button className="border border-blue-400 m-3 p-3" onClick={() => setName('Pedro')}>Mudar nome para Pedro</button>
+      <button className="border border-blue-400 m-3 p-3" onClick={() => setName('João')}>Mudar nome para João</button>
+      <button className="border border-blue-400 m-3 p-3" onClick={() => setAge(30)}>Mudar idade para 30</button>
+      <button className="border border-blue-400 m-3 p-3" onClick={() => setAge(70)}>Mudar idade para 70</button>
     </div>
   );
 }
