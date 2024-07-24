8c405df104887bf52351d18d799cc5d446d566ae
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-24 15:54:41 -0300

    Entendendo o que são Reducers e criando-os

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,16 +1,45 @@
 "use client"
 
-import { Square } from "@/components/Square";
+import { Item } from "@/types/Item";
 import { useState } from "react";
+import { text } from "stream/consumers";
 
 const Page = () => {
-  const [show, setShow] = useState(false)
+  const [list, setList] = useState<Item[]>([])
+
+  const addNewItem = (text: string) => {
+    setList([ ...list, {
+      id: list.length,
+      text,
+      done: false
+    }])
+  }
+
+  const editItemText = (id: number, newText: string) => {
+    setList(
+      list.map(t => {
+        if (t.id === id) t.text = newText;
+        return t;
+      })
+    )
+  }
+
+  const toggleItem = (id: number) => {
+    setList(
+      list.map(t => {
+        if (t.id === id) t.done = !t.done;
+        return t;
+      })
+    )
+  }
+
+  const removeItem = (id: number) => {
+    setList(list.filter(t => t.id !== id))
+  }
 
   return (
     <div className="">
-      <button onClick={() => setShow(!show)}>Mostrar/Ocultar</button>
       
-      {show && <Square />}
     </div>
   );
 }
