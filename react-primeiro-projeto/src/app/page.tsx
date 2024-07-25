9190623a2c5ec1f8e40465cfa197e11e2a94ba7f
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-25 11:17:24 -0300

    Utilizando o Reducer na prática

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,45 +1,83 @@
 "use client"
 
+import { listReducer } from "@/reducers/listReducer";
 import { Item } from "@/types/Item";
-import { useState } from "react";
-import { text } from "stream/consumers";
+import { useReducer, useState } from "react";
 
 const Page = () => {
-  const [list, setList] = useState<Item[]>([])
+  const [list, dispatch] = useReducer(listReducer, []);
+  const [addField, setAddField] = useState('');
 
-  const addNewItem = (text: string) => {
-    setList([ ...list, {
-      id: list.length,
-      text,
-      done: false
-    }])
+  const handleAddButton = () => {
+    if (addField.trim() === '') return false;
+
+    dispatch({
+      type: 'add',
+      payload: { text: addField.trim() }
+    })
+
+    setAddField('')
   }
 
-  const editItemText = (id: number, newText: string) => {
-    setList(
-      list.map(t => {
-        if (t.id === id) t.text = newText;
-        return t;
-      })
-    )
+  const handleDoneCheckbox = (id: number ) => {
+    dispatch({
+      type: 'toggleDone',
+      payload: { id }
+    })
   }
 
-  const toggleItem = (id: number) => {
-    setList(
-      list.map(t => {
-        if (t.id === id) t.done = !t.done;
-        return t;
-      })
-    )
+  const handleEdit = (id: number) => {
+    const item = list.find(it => it.id === id);
+    if (!item) return false;
+
+    const newText = window.prompt('Editar Tarefa', item.text);
+    if (!newText || newText?.trim() === '') return false
+
+    dispatch({
+      type: 'editText',
+      payload: { id, newText }
+    }) 
   }
 
-  const removeItem = (id: number) => {
-    setList(list.filter(t => t.id !== id))
+  const handleRemove = (id: number) => {
+    if(!window.confirm('Deseja realmente excluir esta tarefa?')) return false;
+    dispatch({
+      type: 'remove',
+      payload: { id }
+    })
   }
 
   return (
-    <div className="">
-      
+    <div className="container mx-auto">
+      <h1 className="text-center text-4xl my-4">Lista de Tarefas</h1>
+      <div className="max-w-2xl mx-auto flex rounded-md bg-gray-900 border border-gray-400 p-4 my-4">
+        <input
+          type="text"
+          className="flex-1 rounded-md border border-white p-3 bg-transparent text-white outline-none"
+          placeholder="Digite um item"
+          value={addField}
+          onChange={e => setAddField(e.target.value)}
+        />
+        <button onClick={handleAddButton}>ADICIONAR</button>
+      </div>
+      <ul className="max-w-2xl mx-auto">
+        {list.map(item => (
+          <li 
+            key={item.id}
+            className="flex items-center p-3 my-3 border-b border-gray-700"
+          >
+            <input
+              type="checkbox"
+              className="w-6 h-6 mr-4"
+              checked={item.done}
+              onClick={() => handleDoneCheckbox(item.id)}
+            />
+            <p className="flex-1 text-lg">{item.text}</p>
+            <button onClick={() => handleEdit(item.id)} className="mx-4 text-white hover:text-gray-500">Editar</button>
+            <button onClick={() => handleRemove(item.id)} className="mx-4 text-white hover:text-gray-500">Excluir</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
