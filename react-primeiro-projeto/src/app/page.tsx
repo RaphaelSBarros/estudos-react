6b10f9ae202b7f515b59c9c538f00a7fef8c39b7
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-29 15:43:06 -0300

    Exercício: Chat simples com histórico

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,24 +1,20 @@
 "use client"
 
-import { Button } from "@/components/Button";
-import { Container } from "@/components/Container";
-import { ThemeSwitch } from "@/components/ThemeSwitch";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import { Chat } from "@/components/Chat";
+import { ChatProvider } from "@/contexts/ChatContext";
+import { UserProvider } from "@/contexts/UserContext";
 
 const Page = () => {
   return (
-      <ThemeProvider>
-        <Container>
-          <header className="py-5">
-            <h1 className="text-3xl">Título da Página</h1>
-          </header>
-          <section>
-            <p className="my-5">Conteúdo da Página</p>
-            <Button label="Clique aqui" onClick={() => { }}/>
-          </section>
-          <ThemeSwitch />
-        </Container>
-      </ThemeProvider>
+      <div className="container mx-auto max-w-lg px-2">
+        <UserProvider>
+          <ChatProvider>
+            <h1 className="text-3xl my-3 text-center">Chat Simples</h1>
+
+            <Chat />
+          </ChatProvider>
+        </UserProvider>
+      </div>
   );
 }
 
