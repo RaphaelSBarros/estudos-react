1688bad3a573f2f4dcdf7fb4223ba2eb4358c98f
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-29 11:13:59 -0300

    Exercício: Dark Theme

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
-import { PostList } from "@/components/PostList";
-import { PostProvider } from "@/contexts/PostContext";
+import { Button } from "@/components/Button";
+import { Container } from "@/components/Container";
+import { ThemeSwitch } from "@/components/ThemeSwitch";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const Page = () => {
   return (
-    <PostProvider>
-      <div className="container mx-auto">
-          <Header />
-          <PostList />
-          <Footer />
-      </div>
-    </PostProvider>
+      <ThemeProvider>
+        <Container>
+          <header className="py-5">
+            <h1 className="text-3xl">Título da Página</h1>
+          </header>
+          <section>
+            <p className="my-5">Conteúdo da Página</p>
+            <Button label="Clique aqui" onClick={() => { }}/>
+          </section>
+          <ThemeSwitch />
+        </Container>
+      </ThemeProvider>
   );
 }
 
---
+++ react-primeiro-projeto/src/contexts/ThemeContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+const STORAGE_KEY = 'ThemeContextKey';
+
+type ThemeContext = {
+    theme: string;
+    setTheme: (newTheme: string) => void;
+}
+
+export const ThemeContext = createContext<ThemeContext | null>(null);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const [theme, setTheme] = useState(
+        localStorage.getItem(STORAGE_KEY) || 'light'
+    );
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, theme)
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            { children }
+        </ThemeContext.Provider>
+    );
+}
+
+export const useTheme = () => useContext(ThemeContext);
