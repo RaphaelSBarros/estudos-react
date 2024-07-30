1d7150c7edddc94a7cb318c624db02515b9a3447
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-30 14:16:53 -0300

    Fazendo GET com Axios

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,56 +1,25 @@
 "use client"
 
-import { useRef, useState } from "react";
+import axios from "axios";
 
 const Page = () => {
-  const [legendInput, setLegendInput] = useState('');
-  const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleFileSend = async () => {
-    if (fileInputRef.current?.files && fileInputRef.current.files.length > 0){
-      const fileItem = fileInputRef.current.files[0];
-      const allowed = ['image/jpg', 'image/jpeg', 'image/png']
-
-      if (allowed.includes(fileItem.type)) {
-        const data = new FormData();
-        data.append('image', fileItem)
-        data.append('legend', legendInput)
-
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'multipart/form-data'
-          },
-          body: data
-        });
-        const json = await res.json();
-        console.log(json)
-      } else {
-        alert('Arquivo incompatível')
-      }
-    } else {
-      alert("Selecione um arquivo")
+  const handleGetPosts = async () => {
+    const requestParams = {
+      postId: 1,
+      sort: 'desc'
     }
-  }
 
+    const response = await axios.get('https://jsonplaceholder.typicode.com/comments?postId=1',{
+      params: requestParams
+    });
+    
+    console.log(response.data)
+  }
+  
   return (
       <div className="container mx-auto">
-       <h1 className="text-3xl mt-4">Upload de Imagem</h1>
-
-       <div className="max-w-md flex flex-col gap-3 border border-dotted border-white p-3 mt-4">
-        <input
-          ref={fileInputRef}
-          type="file"
-        />
-        <input 
-          type="text"
-          placeholder="Digite uma legenda"
-          className="p-3 bg-white rounded-md text-black"
-          value={legendInput}
-          onChange={e => setLegendInput(e.target.value)}
-        />
-        <button onClick={handleFileSend}>Enviar Imagem</button>
-       </div>
+        <button onClick={handleGetPosts}>Pegar Posts</button>
       </div>
   );
 }
