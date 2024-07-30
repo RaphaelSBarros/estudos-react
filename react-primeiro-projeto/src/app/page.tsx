6a018e14b1f013070b66fb21558ca67f98d41e23
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-30 09:59:13 -0300

    Deixando a mensagem de loading correta

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -4,6 +4,7 @@ import { User } from "@/types/User";
 import { useEffect, useState } from "react";
 
 const Page = () => {
+  const [loading, setLoading] = useState<boolean>(true);
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
@@ -16,21 +17,22 @@ const Page = () => {
         console.log("DEU ALGUM ERRO")
       })
       .finally(() => {
-        console.log("FINALIZOU TUDO")
+        setLoading(false)
       })
   }, []);
 
   return (
       <div className="container mx-auto">
         <h1 className="text-3xl">Lista de Usuários</h1>
-        {users.length <= 0 && "Carregando..."}
-        {users.length > 0 &&
+        {loading && "Carregando..."}
+        {!loading && users.length > 0 &&
           <ul>
             {users.map(item => (
               <li key={item.id}>{item.name} ({item.email})</li>
             ))}
           </ul>
         } 
+        {!loading && users.length === 0 && "Não há usuários para exibir"}
       </div>
   );
 }
