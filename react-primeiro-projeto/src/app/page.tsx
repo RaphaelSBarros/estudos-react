56d3230b2d3e8fc85b054985d891545bc8311ba6
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-30 09:17:45 -0300

    Fazendo um fetch GET

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,19 +1,20 @@
 "use client"
 
-import { Chat } from "@/components/Chat";
-import { ChatProvider } from "@/contexts/ChatContext";
-import { UserProvider } from "@/contexts/UserContext";
+import { useEffect } from "react";
 
 const Page = () => {
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+      })
+  }, []);
+
   return (
       <div className="container mx-auto max-w-lg px-2">
-        <UserProvider>
-          <ChatProvider>
-            <h1 className="text-3xl my-3 text-center">Chat Simples</h1>
-
-            <Chat />
-          </ChatProvider>
-        </UserProvider>
+        <h1>Lista de Usuários</h1>
       </div>
   );
 }
